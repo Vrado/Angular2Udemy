f1438834134615bb72a08f3d4132c6a99cb726ad
Konrad Kontek <vrado-dev@outlook.de>
2016-08-02 18:40:46 +0200

    Courses

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -3,9 +3,19 @@ import {Component} from 'angular2/core'
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+                <h2>Courses</h2>
+                {{title}}
+                <ul>
+<li *ngFor="#course of courses">
+{{course}}
+</li>
+
+                </ul>
+                `
 
 })
 export class CoursesComponent{
-
+    title ="The title of course page";
+    courses =["Course1", "Course2", "Course3"];
 }
